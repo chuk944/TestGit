dd9649a3aff82340a49e5c11ba50903551837ce2
Charles Kondrath <chuk944@yahoo.com>
2021-01-21 13:26:24 -0800

    Initial Commit

---
+++ Dev2/opengltest2/test2textures/svc/cherno1.cpp
@@ -0,0 +1,137 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "Renderer.h"
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "Shader.h"
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+	{
+		float positions[] = {
+			-0.5f, -0.5f, // 0
+			 0.5f, -0.5f, // 1
+			 0.5f,  0.5f, // 2
+			-0.5f,  0.5f  // 3
+		};
+
+		unsigned int indices[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+
+
+		VertexArray va;
+		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
+
+		IndexBuffer ib(indices, 6);
+
+		Shader shader("res/shaders/Basic.shader");
+		shader.Bind();
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.SetUniform1f("u_factorx", 0.0f);
+		shader.SetUniform1f("u_factory", 0.0f);
+
+		va.Unbind();
+		vb.Unbind();
+		ib.Unbind();
+		shader.Unbind();
+
+		Renderer renderer;
+
+
+		float r = 0.0f;
+		float increment = 0.05f;
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			renderer.Clear();
+
+			shader.Bind();
+			shader.SetUniform4f("u_Color", 0.0f, 0.3f, 0.8f, 1.0f);
+			shader.SetUniform1f("u_factorx", 0.5f);
+			shader.SetUniform1f("u_factory", 0.5f);
+			renderer.Draw(va, ib, shader);
+
+
+
+			shader.SetUniform4f("u_Color", 0.0f, 0.9f, 0.0f, 1.0f);
+			shader.SetUniform1f("u_factorx", 0.0f);
+			shader.SetUniform1f("u_factory", 0.0f);
+			renderer.Draw(va, ib, shader);
+
+			shader.SetUniform4f("u_Color", 1.0f, 0.0f, 0.0f, 1.0f);
+			shader.SetUniform1f("u_factorx",-0.5f);
+			shader.SetUniform1f("u_factory",-0.5f);
+			renderer.Draw(va, ib, shader);
+
+
+			shader.Unbind();
+			glLoadIdentity();
+			glColor3f(r, r, 1.0);
+			glTranslated(-1.0, 0.0, 0.0);
+			glScalef(0.0017, 0.0017, 0.0);
+			glLineWidth(5);
+			glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"Donna is Beautiful");
+
+			if (r > 1.0f)
+				increment = -0.05f;
+			else if (r < 0.0f)
+				increment = 0.05f;
+
+			r += increment;
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+
+	}
+	glfwTerminate();
+	return 0;
+}
